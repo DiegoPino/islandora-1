bff8bbf42f98fc9f1c99c3fba6348695d8690a34
Diego Pino Navarro <dpino@metro.org>
2017-03-22 11:44:53 -0400

    Create Context generator Kernel tests

---
+++ tests/src/Kernel/JsonldContextGeneratorTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\Tests\islandora\Kernel;
+
+/**
+ * Tests the Json-LD context Generator methods and simple integration.
+ *
+ * @group islandora
+ * @coversDefaultClass \Drupal\islandora\RdfBundleSolver\JsonldContextGenerator
+ */
+class JsonldContextGeneratorTest extends IslandoraKernelTestBase  {
+
+  /**
+   * @covers \Drupal\islandora\RdfBundleSolver\JsonldContextGenerator::getContext
+   */
+}
+
